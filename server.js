c50a886a4b42a17b05ca39a7cbc23f955b7a4368
Varada-Echidna <varada.v>
2016-08-19 18:35:41 +0530

    Next commit.

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to your fob first api!' });
+	res.json({ message: 'Hi Suhas !!!> hooray! welcome to your fob first api!' });
 });
 
 // on routes that end in /bears
